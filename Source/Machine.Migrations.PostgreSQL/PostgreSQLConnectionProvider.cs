081c862e0b29b02c4c98a81e0ee5aceafc295bd8
Jeff Zellner <jeff@odyl.net>
2011-03-15 14:11:32 -0400

    initial commit of postgres support

---
+++ Source/Machine.Migrations.PostgreSQL/PostgreSQLConnectionProvider.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using Machine.Migrations.Services;
+using Machine.Migrations.Services.Impl;
+using Npgsql;
+
+namespace Machine.Migrations.PostgreSQL
+{
+    public class PostgreSQLConnectionProvider : AbstractConnectionProvider
+    {
+        public PostgreSQLConnectionProvider(IConfiguration configuration)
+            : base(configuration)
+        {
+        }
+
+        protected override IDbConnection CreateConnection(IConfiguration configuration, string key)
+        {
+            return new NpgsqlConnection(configuration.ConnectionStringByKey(key));
+        }
+    }
+}
---
+++ Source/Machine.Migrations.PostgreSQL/PostgreSQLSchemaProvider.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Machine.Migrations.SchemaProviders;
+
+namespace Machine.Migrations.PostgreSQL
+{
+    public class PostgreSQLSchemaProvider : ISchemaProvider
+    {
+        public void AddTable(string table, ICollection<Column> columns)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DropTable(string table)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RenameTable(string table, string newName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void AddColumn(string table, string column, Type type)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void AddColumn(string table, string column, Type type, bool allowNull)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void AddColumn(string table, string column, Type type, short size, bool isPrimaryKey, bool allowNull)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void AddColumn(string table, string column, Type type, short size, bool allowNull)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RemoveColumn(string table, string column)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RenameColumn(string table, string column, string newName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void AddSchema(string name)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RemoveSchema(string name)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool HasTable(string table)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool HasColumn(string table, string column)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool HasSchema(string name)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ChangeColumn(string table, string column, Type type, short size, bool allowNull)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string[] Columns(string table)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string[] Tables()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void AddForeignKeyConstraint(string table, string name, string column, string foreignTable, string foreignColumn)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void AddUniqueConstraint(string table, string name, params string[] columns)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DropConstraint(string table, string name)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool IsColumnOfType(string table, string column, string type)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
